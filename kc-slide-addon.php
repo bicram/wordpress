93cd0a8b61bf981c3fe884130844adf592d7aa88
Shahadat Shuvo <shuvombm02@gmail.com>
2018-05-26 00:22:03 +0600

    Update kc-slide-addon.php

--- kc-slide-addon.php
+++ kc-slide-addon.php
@@ -23,18 +23,18 @@ function kc_slides() {
 	                        'label' =>esc_html__('Slide amount'),
 	                        'type' => 'select',
 	                        'options' => array(  
-                              '-1' => esc_html__('All Slide'),
-                              '1' => esc_html__('1 Slide'),
-                              '2' => esc_html__('2 Slide'),
-                              '3' => esc_html__('3 Slide'),
-                              '4' => esc_html__('4 Slide'),
-                              '5' => esc_html__('5 Slide'),
-                              '6' => esc_html__('6 Slide'),
-                              '7' => esc_html__('7 Slide'),
-                              '8' => esc_html__('8 Slide'),
-                              '9' => esc_html__('9 Slide'),
-                              '10' => esc_html__('10Slide'),
-                            ),
+				      '-1' => esc_html__('All Slide'),
+				      '1' => esc_html__('1 Slide'),
+				      '2' => esc_html__('2 Slide'),
+				      '3' => esc_html__('3 Slide'),
+				      '4' => esc_html__('4 Slide'),
+				      '5' => esc_html__('5 Slide'),
+				      '6' => esc_html__('6 Slide'),
+				      '7' => esc_html__('7 Slide'),
+				      '8' => esc_html__('8 Slide'),
+				      '9' => esc_html__('9 Slide'),
+				      '10' => esc_html__('10Slide'),
+				    ),
 	                        'value' => -1,
 	                        'description' => esc_html__('Select how many slide you want to show.', 'wtint-shuvombm')
 	                    ),
@@ -45,56 +45,56 @@ function kc_slides() {
 	                        'type' => 'select',
 							            'options' => get_slider_list(),
 	                        'relation' => array(
-                            'parent'    => 'count',
-                            'show_when' => '1'
-                        )
+				    'parent'    => 'count',
+				    'show_when' => '1'
+				)
 	                    ),
 	                    array(
 	                        'name' => 'loop',
 	                        'label' =>esc_html__('Enable loop?'),
 	                        'type' => 'select',  
-                          'options' => array(  
-                            'true' => esc_html__('Yes'),
-                            'false' => esc_html__('No'),
-                          ),
+				  'options' => array(  
+				    'true' => esc_html__('Yes'),
+				    'false' => esc_html__('No'),
+				  ),
 						 
-							            'value' => 'true',
+				 'value' => 'true',
 	                        'description' => esc_html__('If you want loop then select yes, else select no', 'wtint-shuvombm')
 	                    ),
 	                    array(
 	                        'name' => 'autoplay',
 	                        'label' => esc_html__('Enable autoplay?'),
 	                        'type' => 'select',
-                          'options' => array(  
-                            'true' => esc_html__('Yes'),
-                            'false' => esc_html__('No'),
-                          ),
+				  'options' => array(  
+				    'true' => esc_html__('Yes'),
+				    'false' => esc_html__('No'),
+				  ),
 
-                          'value' => 'true',
+                          	'value' => 'true',
 	                        'description' => esc_html__('If you want autoplay then select yes, else select no', 'wtint-shuvombm')
 	                    ),
 	                    array(
 	                        'name' => 'nav',
 	                        'label' => esc_html__('Enable nav?'),
 	                        'type' => 'select', 
-                          'options' => array(  
-                            'true' => esc_html__('Yes'),
-                            'false' => esc_html__('No'),
-                          ),
+				  'options' => array(  
+				    'true' => esc_html__('Yes'),
+				    'false' => esc_html__('No'),
+				  ),
 
-                          'value' => 'true',
+                          	'value' => 'true',
 	                        'description' => esc_html__('If you want nav then select yes, else select no', 'wtint-shuvombm')
 	                    ),
 	                    array(
 	                        'name' => 'dots',
 	                        'label' => esc_html__('Enable dots?'),
 	                        'type' => 'select',
-                          'options' => array(  
-                            'true' => esc_html__('Yes'),
-                            'false' => esc_html__('No'),
-                          ),
+				  'options' => array(  
+				    'true' => esc_html__('Yes'),
+				    'false' => esc_html__('No'),
+				  ),
 
-                          'value' => 'true',
+                          	'value' => 'true',
 	                        'description' => esc_html__('If you want dots then select yes, else select no', 'wtint-shuvombm')
 	                    )
 	                )
